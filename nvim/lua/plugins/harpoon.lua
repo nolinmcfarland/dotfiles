d3478d394dc752dba2b876eb567c10fee288e7cf
Nolin McFarland <nolin.mcfarland@icloud.com>
2025-04-15 10:34:36 -0400

    Set better harpoon mappings

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -7,11 +7,11 @@ return {
 		harpoon:setup()
 
 		vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-		vim.keymap.set("n", "<C-h>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+		vim.keymap.set("n", "<C-5>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-		vim.keymap.set("n", "<C-7>", function() harpoon:list():select(1) end)
-		vim.keymap.set("n", "<C-8>", function() harpoon:list():select(2) end)
-		vim.keymap.set("n", "<C-9>", function() harpoon:list():select(3) end)
-		vim.keymap.set("n", "<C-0>", function() harpoon:list():select(4) end)
+		vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
+		vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end)
+		vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end)
+		vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end)
 	end
 }
