6cd82ad4b0e5e3c7a4bf5aec0dd604368e8f6bc0
Nolin McFarland <nolin.mcfarland@icloud.com>
2025-04-15 10:36:38 -0400

    Improve mappings

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,14 +1,16 @@
 return {
-	'nvim-telescope/telescope.nvim', tag = '0.1.8',
-	dependencies = {
-		'nvim-lua/plenary.nvim'
-	},
-	config = function()
-		require('telescope').setup({})
-		local builtin = require('telescope.builtin')
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    dependencies = {
+        'nvim-lua/plenary.nvim'
+    },
+    config = function()
+        require('telescope').setup({})
+        local builtin = require('telescope.builtin')
 
-		vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-		vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
-	end
+        vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+        vim.keymap.set("n", "<leader>pg", builtin.git_files, {})
+        vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
+
+    end
 }
 
