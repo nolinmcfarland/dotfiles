fcbc68baf0a2e5b1b6c95ebbf13fc09754e54b6f
Nolin McFarland <nolin.mcfarland@icloud.com>
2025-04-15 10:26:28 -0400

    Update nvim config

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -1,4 +1,3 @@
 vim.g.mapleader = " "
 
--- Project view
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
--- nvim/lua/config/set.lua
+++ nvim/lua/config/set.lua
@@ -9,6 +9,7 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
+vim.opt.spellsuggest = "best,9"
 
 vim.opt.wrap = false
 
@@ -17,7 +18,7 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
-vim.opt.hlsearch = true
+vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
@@ -29,5 +30,3 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
-
-vim.opt.mouse = ""
