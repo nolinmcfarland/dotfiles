ad2ddd80028daead56d3eb6479b7c07e396fa936
Nolin McFarland <nolin.mcfarland@icloud.com>
2025-04-24 10:57:20 -0400

    Add Swift author header autocmd

--- nvim/lua/config/autocmd.lua
+++ nvim/lua/config/autocmd.lua
@@ -1,7 +1,31 @@
-vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = 'Highlight when yanking (copying) text',
-    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-    callback = function()
-        vim.highlight.on_yank()
-    end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+	desc = "Add Xcode-style author info to the top of new Swift files",
+	pattern = "*.swift",
+	callback = function()
+		if vim.api.nvim_buf_line_count(0) > 1 or vim.api.nvim_get_current_line() ~= "" then
+			return
+		end
+		local filename = vim.fn.expand("%:t")
+		local author = vim.fn.system("git config user.name"):gsub("\n", "")
+		local date = os.date("%m/%d/%y")
+		local lines = {
+			"//",
+			"//  " .. filename,
+			"//",
+			"//",
+			"//  Created by " .. author .. " on " .. date .. ".",
+			"//",
+			"",
+		}
+
+		vim.api.nvim_buf_set_lines(0, 0, 0, false, lines)
+	end,
 })
