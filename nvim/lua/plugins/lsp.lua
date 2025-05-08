a04767d599f30cbddcf712791c86986e97f20f7c
Nolin McFarland <nolin.mcfarland@icloud.com>
2025-05-08 18:04:17 -0400

    Fix LSP for Neovim v0.11

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -37,30 +37,6 @@ return {
 				map("[d", vim.diagnostic.goto_prev, "Goto Previous Diagnostic")
 
 				map("]d", vim.diagnostic.goto_prev, "Goto Next Diagnostic")
-
-				local client = vim.lsp.get_client_by_id(event.data.client_id)
-				if client then
-					local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
-					vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-						buffer = event.buf,
-						group = highlight_augroup,
-						callback = vim.lsp.buf.document_highlight,
-					})
-
-					vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-						buffer = event.buf,
-						group = highlight_augroup,
-						callback = vim.lsp.buf.clear_references,
-					})
-
-					vim.api.nvim_create_autocmd("LspDetach", {
-						group = vim.api.nvim_create_augroup("lsp-detach", { clear = true }),
-						callback = function(event2)
-							vim.lsp.buf.clear_references()
-							vim.api.nvim_clear_autocmds({ group = "lsp-highlight", buffer = event2.buf })
-						end,
-					})
-				end
 			end,
 		})
 
