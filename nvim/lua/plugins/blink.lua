73c0cb4f30c06d94562c6350313e5f8c5c20a19d
Nolin McFarland <nolin.mcfarland@icloud.com>
2025-05-12 09:51:34 -0400

    Remove unneeded plugins

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,58 +1,64 @@
 return {
-    'saghen/blink.cmp',
-    event = 'VimEnter',
-    version = '1.*',
-    dependencies = {
-        {
-            'L3MON4D3/LuaSnip',
-            version = '2.*',
-            build = (function()
-                return 'make install_jsregexp'
-            end)(),
-            dependencies = {
-                {
-                    'rafamadriz/friendly-snippets',
-                    config = function()
-                        require('luasnip.loaders.from_vscode').lazy_load()
-                    end,
-                },
-            },
-            opts = {},
-        },
-        'folke/lazydev.nvim',
-    },
-    --- @module 'blink.cmp'
-    --- @type blink.cmp.Config
-    opts = {
-        keymap = {
-            -- <c-y> to accept ([y]es) the completion.
-            -- <tab>/<s-tab>: move to right/left of your snippet expansion
-            -- <c-space>: Open menu or open docs if already open
-            -- <c-n>/<c-p> or <up>/<down>: Select next/previous item
-            -- <c-e>: Hide menu
-            -- <c-k>: Toggle signature help
-            preset = 'default',
-        },
+	"saghen/blink.cmp",
+	event = "VimEnter",
+	version = "1.*",
+	dependencies = {
+		{
+			"L3MON4D3/LuaSnip",
+			version = "2.*",
+			build = (function()
+				return "make install_jsregexp"
+			end)(),
+			dependencies = {
+				{
+					"rafamadriz/friendly-snippets",
+					config = function()
+						require("luasnip.loaders.from_vscode").lazy_load()
+					end,
+				},
+			},
+			opts = {},
+		},
+		"folke/lazydev.nvim",
+	},
+	--- @module 'blink.cmp'
+	--- @type blink.cmp.Config
+	opts = {
+		keymap = {
+			-- <c-y> to accept ([y]es) the completion.
+			-- <tab>/<s-tab>: move to right/left of your snippet expansion
+			-- <c-space>: Open menu or open docs if already open
+			-- <c-n>/<c-p> or <up>/<down>: Select next/previous item
+			-- <c-e>: Hide menu
+			-- <c-k>: Toggle signature help
+			preset = "default",
+		},
 
-        appearance = {
-            nerd_font_variant = 'mono',
-        },
+		appearance = {
+			nerd_font_variant = "mono",
+		},
 
-        completion = {
-            documentation = { auto_show = false, auto_show_delay_ms = 500 },
-        },
+		completion = {
+			documentation = { auto_show = false, auto_show_delay_ms = 500 },
+			list = {
+				selection = {
+					preselect = true,
+					auto_insert = true,
+				},
+			},
+		},
 
-        sources = {
-            default = { 'lsp', 'path', 'snippets', 'lazydev' },
-            providers = {
-                lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
-            },
-        },
+		sources = {
+			default = { "lsp", "path", "snippets", "lazydev" },
+			providers = {
+				lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
+			},
+		},
 
-        snippets = { preset = 'luasnip' },
+		snippets = { preset = "luasnip" },
 
-        fuzzy = { implementation = 'lua' },
+		fuzzy = { implementation = "lua" },
 
-        signature = { enabled = true },
-    },
+		signature = { enabled = true },
+	},
 }
