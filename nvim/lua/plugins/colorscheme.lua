9130cfec9a82c29122c4195ce2a79f5f1236eac7
Nolin McFarland <nolin.mcfarland@gmail.com>
2026-07-24 21:05:32 -0400

    feat: update config

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -3,15 +3,8 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-        require('ayu').setup({
-            overrides = {
-                Comment = {
-                    italic = true,
-                },
-            },
-        })
-        vim.cmd.colorscheme('ayu-mirage')
-        vim.o.winborder = "rounded"
+        require('ayu').setup({})
+        vim.cmd.colorscheme('ayu')
+        vim.o.winborder = 'rounded'
     end,
 }
-
--- nvim/lua/plugins/todo.lua
+++ nvim/lua/plugins/todo.lua
@@ -4,13 +4,13 @@ return {
     config = function()
         require('todo-comments').setup()
 
-        vim.keymap.set("n", "]t", function()
-            require("todo-comments").jump_next()
-        end, { desc = "Next todo comment" })
+        vim.keymap.set('n', ']t', function()
+            require('todo-comments').jump_next()
+        end, { desc = 'Next todo comment' })
 
-        vim.keymap.set("n", "[t", function()
-            require("todo-comments").jump_prev()
-        end, { desc = "Previous todo comment" })
+        vim.keymap.set('n', '[t', function()
+            require('todo-comments').jump_prev()
+        end, { desc = 'Previous todo comment' })
 
         vim.keymap.set('n', '<leader>st', '<CMD>TodoTelescope<CR>', { desc = '[S]earch [T]odos' })
     end,
