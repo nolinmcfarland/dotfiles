cccdba80e8c780c8247d16d1d3c39c00d49902ec
Nolin McFarland <nolin.mcfarland@gmail.com>
2026-08-19 07:35:39 -0400

    feat: update config

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,9 @@
 return {
-    'Shatur/neovim-ayu',
+    'projekt0n/github-nvim-theme',
     lazy = false,
     priority = 1000,
     config = function()
-        require('ayu').setup({})
-        vim.cmd.colorscheme('ayu')
+        vim.cmd.colorscheme('github_dark_default')
         vim.o.winborder = 'rounded'
     end,
 }
